eb67137b6c0d581f7a112a9ae70ee2ec09f6e458
FRMS <frms@tamu.edu>
2025-08-19 16:54:59 -0500

    working on frontend
    testing frontend deployment and docker building

--- KaleidoscopeBackEnd/main.go
+++ KaleidoscopeBackEnd/main.go
@@ -11,17 +11,18 @@ import (
 )
 
 func main() {
-	_, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
 		log.Fatal("MONGO_URI not set")
 	}
-
 	defer cancel()
 	_, err := mongo.Connect(options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print("Connected no issues ---------------------")
+
+	log.Print("Connected, no issues ---------------------")
+
 }
