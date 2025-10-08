1d59ab4ef268aae4b34bb0b5010287d6640e848d
FRMS <robin.schniebel@outlook.com>
2025-10-08 18:23:04 -0500

    menu bar

--- KaleidoscopeFrontend/src/App.tsx
+++ KaleidoscopeFrontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
+import Navlayout from './NavLayout.tsx'
+import backGround from './assets/random Hexa.png'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-      
-    </>
+    <div className=" w-full"> 
+      <div className="relative h-dvh w-full overflow-hidden 4xl:w-6/10 justify-self-center">
+        <img
+          src={backGround}
+          alt="background"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+        <Navlayout></Navlayout>
+      </div>
+    </div>
   )
 }
 
